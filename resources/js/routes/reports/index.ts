5f797f350282fd5b3309f1b4ec53e90ffdbf5591
Fatihza Raj Sandy Tanjung <rajsandyt1@gmail.com>
2026-06-01 15:09:14 +0700

    fix: install date-fns and update wayfinder routes

--- resources/js/routes/reports/index.ts
+++ resources/js/routes/reports/index.ts
@@ -1,8 +1,89 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../wayfinder'
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+
+index.definition = {
+    methods: ["get","head"],
+    url: '/customer-service',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+index.url = (options?: RouteQueryOptions) => {
+    return index.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: index.url(options),
+    method: 'head',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: index.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: index.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::index
+* @see app/Http/Controllers/ReportController.php:14
+* @route '/customer-service'
+*/
+indexForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: index.url({
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+index.form = indexForm
+
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 export const create = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: create.url(options),
@@ -11,13 +92,13 @@ export const create = (options?: RouteQueryOptions): RouteDefinition<'get'> => (
 
 create.definition = {
     methods: ["get","head"],
-    url: '/customer-service',
+    url: '/customer-service/create',
 } satisfies RouteDefinition<["get","head"]>
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 create.url = (options?: RouteQueryOptions) => {
     return create.definition.url + queryParams(options)
@@ -25,8 +106,8 @@ create.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 create.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: create.url(options),
@@ -35,8 +116,8 @@ create.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 create.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: create.url(options),
@@ -45,8 +126,8 @@ create.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 const createForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
     action: create.url(options),
@@ -55,8 +136,8 @@ const createForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => 
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 createForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
     action: create.url(options),
@@ -65,8 +146,8 @@ createForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
 
 /**
 * @see \App\Http\Controllers\ReportController::create
-* @see app/Http/Controllers/ReportController.php:13
-* @route '/customer-service'
+* @see app/Http/Controllers/ReportController.php:21
+* @route '/customer-service/create'
 */
 createForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
     action: create.url({
@@ -82,7 +163,7 @@ create.form = createForm
 
 /**
 * @see \App\Http\Controllers\ReportController::store
-* @see app/Http/Controllers/ReportController.php:18
+* @see app/Http/Controllers/ReportController.php:33
 * @route '/customer-service'
 */
 export const store = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -97,7 +178,7 @@ store.definition = {
 
 /**
 * @see \App\Http\Controllers\ReportController::store
-* @see app/Http/Controllers/ReportController.php:18
+* @see app/Http/Controllers/ReportController.php:33
 * @route '/customer-service'
 */
 store.url = (options?: RouteQueryOptions) => {
@@ -106,7 +187,7 @@ store.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\ReportController::store
-* @see app/Http/Controllers/ReportController.php:18
+* @see app/Http/Controllers/ReportController.php:33
 * @route '/customer-service'
 */
 store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -116,7 +197,7 @@ store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
 
 /**
 * @see \App\Http\Controllers\ReportController::store
-* @see app/Http/Controllers/ReportController.php:18
+* @see app/Http/Controllers/ReportController.php:33
 * @route '/customer-service'
 */
 const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
@@ -126,7 +207,7 @@ const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => 
 
 /**
 * @see \App\Http\Controllers\ReportController::store
-* @see app/Http/Controllers/ReportController.php:18
+* @see app/Http/Controllers/ReportController.php:33
 * @route '/customer-service'
 */
 storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
@@ -136,9 +217,116 @@ storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => (
 
 store.form = storeForm
 
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+export const show = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: show.url(args, options),
+    method: 'get',
+})
+
+show.definition = {
+    methods: ["get","head"],
+    url: '/customer-service/{report}',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+show.url = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { report: args }
+    }
+
+    if (typeof args === 'object' && !Array.isArray(args) && 'id' in args) {
+        args = { report: args.id }
+    }
+
+    if (Array.isArray(args)) {
+        args = {
+            report: args[0],
+        }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+        report: typeof args.report === 'object'
+        ? args.report.id
+        : args.report,
+    }
+
+    return show.definition.url
+            .replace('{report}', parsedArgs.report.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+show.get = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: show.url(args, options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+show.head = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: show.url(args, options),
+    method: 'head',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+const showForm = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: show.url(args, options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+showForm.get = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: show.url(args, options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\ReportController::show
+* @see app/Http/Controllers/ReportController.php:26
+* @route '/customer-service/{report}'
+*/
+showForm.head = (args: { report: number | { id: number } } | [report: number | { id: number } ] | number | { id: number }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: show.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+show.form = showForm
+
 const reports = {
+    index: Object.assign(index, index),
     create: Object.assign(create, create),
     store: Object.assign(store, store),
+    show: Object.assign(show, show),
 }
 
 export default reports
